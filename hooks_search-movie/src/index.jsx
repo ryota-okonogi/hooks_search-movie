24fc0f0be37ac05e91feb9264f5a99f76b98d63a
nogi5000 <R5000.power@gmail.com>
2022-10-28 00:20:22 +0900

    Minor fix

--- hooks_search-movie/src/index.jsx
+++ hooks_search-movie/src/index.jsx
@@ -5,12 +5,7 @@ import { apikey } from './lib/entity';
 
 const container = document.getElementById('root');
 
-// `/hoge?api_key=${process.env.DEMO_API_KEY}`
-// `/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
-// const appId = process.env.REACT_APP_MOVIE_API_KEY
-// {process.env.REACT_APP_HELLO_WORLD}
-
-// renderメソッドで、　「HTML(index.html)上にある'root'というidが振られているDOMを <App /> に置き換える」ということをやっている。
+// renderメソッドで、「HTML(index.html)上にある'root'というidが振られているDOMを <App /> に置き換える」ということをやっている。
 
 if (container) {
 const root = createRoot(container);
